d6f85954a6391253c4d3eae6989b3f5ac6b42ea4
Ahajiwoe <108989865+Ahajiwoe@users.noreply.github.com>
2022-08-27 16:45:08 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket2"
+    bucket = "my-dev-tf-state-bucket-haji"
     key = "main"
     region = "us-east-2"
     dynamodb_table = "my-dynamodb-table"
